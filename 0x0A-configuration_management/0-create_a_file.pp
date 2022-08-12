75aaf266ec27b4ae415361ec3437821017897485
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-12 17:50:03 +0000

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp
 file { '/tmp/school':
-    mode => '0774', 
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0774',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }

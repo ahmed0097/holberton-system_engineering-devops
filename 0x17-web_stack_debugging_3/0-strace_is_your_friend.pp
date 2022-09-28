23c4e160c45bdf768ddddf2a78b3d20ab0b6c4ad
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-28 01:33:51 +0100

    strace_is_your_friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fix 500 Apache error
+exec { 'fix-typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}

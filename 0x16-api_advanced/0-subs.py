8a15473bcd6b4d1011b9d60870f31375d335ad5a
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-28 02:21:01 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     subreds = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     red_req = requests.get(subreds, headers={'User-Agent': 'lee'})
     if (red_req.status_code == requests.codes.ok):
-        jason_full = red_req.json()
-        return (jason_full['data']['subscribers'])
+        json_full = red_req.json()
+        return (json_full['data']['subscribers'])
     else:
         return (0)
